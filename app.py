e79ca5ffdef505db7f87035332688d8a7727e963
Piyush Sharma <piyush.blue8585@gmail.com>
2025-07-27 22:14:43 +0800

    test3

--- app.py
+++ app.py
@@ -1,27 +1,58 @@
 from flask import Flask, render_template, request, jsonify
-import vertexai
-from vertexai.language_models import ChatModel
+#import vertexai
+#from vertexai.language_models import ChatModel
 import os
+import google.generativeai as genai
 
 app = Flask(__name__)
 PROJECT_ID = "project-dev-1985"  
-LOCATION = "us-central1"  
+LOCATION = "us-central1" 
 
-vertexai.init(project=PROJECT_ID, location=LOCATION)
+GOOGLE_API_KEY='AIzaSyBl5oCGT1lAhH_EJsdB2xOCfCOwXd0fwrY'
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+
+#vertexai.init(project=PROJECT_ID, location=LOCATION)
 
 def create_session():
-    chat_model = ChatModel.from_pretrained("chat-bison@001")
-    chat = chat_model.start_chat()
+    #chat_model = ChatModel.from_pretrained("chat-bison@001")
+    generation_config = {
+                    "temperature": 0.7,
+                    "top_p": 0.9,
+                    "top_k": 20,
+                    "max_output_tokens": 1024
+                    }
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash",
+                              generation_config=generation_config)
+    chat = model.start_chat()
     return chat
 
 def response(chat, message):
-    parameters = {
-        "temperature": 0.2,
-        "max_output_tokens": 256,
-        "top_p": 0.8,
-        "top_k": 40
-    }
-    result = chat.send_message(message, **parameters)
+    #parameters = {
+     #   "temperature": 0.2,
+      #  "max_output_tokens": 256,
+       # "top_p": 0.8,
+        #"top_k": 40
+    #}
+    generation_config = {
+                    "temperature": 0.7,
+                    "top_p": 0.9,
+                    "top_k": 20,
+                    "max_output_tokens": 1024
+                    }
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash",
+                              generation_config=generation_config)
+    #result = chat.send_message(message, **parameters)
+
+    prompt = f"""
+        You are a senior story writer. Based on the user's request, 
+        suggest potential stories for 200 words.
+         the stories should be funny and creative.
+        """
+    
+    result = model.generate_content(prompt)
+    #result = chat.send_message(message, generation_config)
     return result.text
 
 @app.route('/')
